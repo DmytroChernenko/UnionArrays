258e689dab3cdc5f52d9d31188f33cb9abf29efd
DmytroChernenko <dmytro.chernenko@ex.ua>
2015-09-18 11:28:17 +0300

    Initial commit

---
+++ src/com/company/ArrayUnion.java
@@ -0,0 +1,69 @@
+package com.company;
+
+import java.util.Arrays;
+
+/**
+ * Created by Dmytro on 18.09.15.
+ */
+public class ArrayUnion {
+
+    private int[] left;
+    private int[] right;
+
+    public ArrayUnion(int[] left,int[] right){
+        this.left = left;
+        this.right = right;
+    }
+
+    public ArrayUnion() {
+    }
+
+    public int[] leftUnion(int[] left, int[] right) {
+
+        int length = left.length;
+
+        int[] copy = Arrays.copyOf(left, left.length);
+
+
+        int count = 0;
+
+        Arrays.sort(copy);
+
+        for (int i : right) {
+            int index = Arrays.binarySearch(copy, i);
+            if(index > -1)
+                count++;
+        }
+
+        length += count;
+
+
+        int[] result = Arrays.copyOf(left, length);
+
+        int i = left.length;
+
+        for (int j = 0; i < result.length; j++) {
+            int index = Arrays.binarySearch(copy, right[j]);
+            if(index > -1) {
+                result[i] = right[j];
+                i++;
+            }
+        }
+
+        return result;
+    }
+
+
+    public static boolean checkArrays(int[] result, int[] true_result) {
+        if( result.length != true_result.length)
+            return false;
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != true_result[i])
+                return false;
+        }
+
+        return true;
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int[] left = {1, 5, 4, 23, 65, 32, 78};
+        int[] right = {3, 5, 24, 4, 1, 2, 34, 45, 32, 5};
+
+        final int[] TRUE_RESULT = {1, 5, 4, 23, 65, 32, 78, 5, 4, 1, 32, 5};
+
+        ArrayUnion arrayUnion = new ArrayUnion();
+
+        int[] result = arrayUnion.leftUnion(left, right);
+
+        System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
+        System.out.println(Arrays.toString(result));
+        System.out.println(Arrays.toString(TRUE_RESULT));
+
+        boolean isResultTrue = ArrayUnion.checkArrays(result, TRUE_RESULT);
+
+        System.out.println("isResultTrue = " + isResultTrue);
+
+
+    }
+}
